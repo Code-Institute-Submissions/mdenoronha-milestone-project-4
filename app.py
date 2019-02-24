6fb35d48d5018dbaf6d8de891a593f591418c8b4
Matthew <matthewdenoronha@gmail.com>
2019-02-24 15:22:34 +0000

    Add functionality to upload images
    This has been placed in a separate function (upload_file()) but will soon be
    part of the add and update recipe feature.

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 import os
+from werkzeug.utils import secure_filename
+from os.path import join, dirname, realpath
+
+# Assistance in file path from https://stackoverflow.com/questions/37901716/flask-uploads-ioerror-errno-2-no-such-file-or-directory
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'assets/recipe-images/..')
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+# Assistance in file path from https://stackoverflow.com/questions/37901716/flask-uploads-ioerror-errno-2-no-such-file-or-directory
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///recipeapp.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 recipe_ingredients = db.Table('recipe_ingredients',
     db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
     db.Column('ingredients_id', db.Integer, db.ForeignKey('ingredients.id')),
-    db.Column('measurements_id', db.Integer, db.ForeignKey('measurements.id'))
 )
 
 class Recipe(db.Model):
@@ -24,25 +37,17 @@ class Recipe(db.Model):
     method = db.Column(db.Text, nullable=False)
     ingredients = db.relationship('Ingredients', secondary=recipe_ingredients, lazy='subquery',
         backref=db.backref('ingredients', lazy=True))
-    measurements = db.relationship('Measurements', secondary=recipe_ingredients, lazy='subquery',
-        backref=db.backref('measurements', lazy=True))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
         nullable=False)    
 
     def __repr__(self):
         return '<Recipe %r>' % (self.id)
-            
-class Measurements(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    unit = db.Column(db.String(80), nullable=False)
-    amount = db.Column(db.String(80), nullable=False)
-    
-    def __repr__(self):
-        return '<Measurements %r>' % self.unit
         
 class Ingredients(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    unit = db.Column(db.String(80), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
     is_vegetarian = db.Column(db.Boolean, nullable=False)
     is_vegan = db.Column(db.Boolean, nullable=False)
     is_gluten_free = db.Column(db.Boolean, nullable=False)
@@ -75,6 +80,40 @@ def index():
 
      
     return render_template('index.html', recipe_text=recipe_text)
+
+# http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+    
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
